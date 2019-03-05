08f714b47df30c5965b606f1383d88bb34b37128
ianba <ianbabington93@gmail.com>
2019-03-05 20:17:30 +1300

    initial commit

---
+++ library/src/main/java/library/config/RootConfig.java
@@ -0,0 +1,14 @@
+package library.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages= {"library"}, 
+		excludeFilters={@Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)})
+public class RootConfig {
+	
+}
---
+++ library/src/main/java/library/config/WebConfig.java
@@ -0,0 +1,33 @@
+package library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("library.web")
+public class WebConfig extends WebMvcConfigurerAdapter{	//TODO: use non-deprecated 
+	
+	@Bean
+	public ViewResolver viewResolver() { //TODO: is there a better resolver I could use?
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	
+	
+}
